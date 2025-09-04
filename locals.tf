47f5672cff719f0122f34267380ff270928dae0d
Federico Midaglia <fmidaglia@gocloud.la>
2025-09-04 14:32:26 -0300

    feat(module): metadata refactor

--- locals.tf
+++ locals.tf
@@ -1,18 +1,4 @@
 locals {
-  metadata = var.metadata
-
-  common_name = join("-", [
-    local.metadata.key.company,
-    local.metadata.key.env
-  ])
-
-  common_tags = {
-    "company"     = local.metadata.key.company
-    "provisioner" = "terraform"
-    "environment" = local.metadata.environment
-    "created-by"  = "GoCloud.la"
-  }
-
   service_scheduler_enable = lookup(var.service_scheduler_parameters, "enable", false) ? 1 : 0
 
   power_on_schedule_enable  = try(var.service_scheduler_parameters.power_on_schedule, null)
